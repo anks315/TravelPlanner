4d2c7364da964195bddb0cbe73cc4b0a86e616df
shekhar1989 <shekhar.gupta@hotmail.com>
2016-06-01 07:42:20 +0530

    rioutemap

---
+++ GoIndi/static/js/routeMap.js
@@ -0,0 +1,18 @@
+var routeMap = {}
+var flightRouteList
+function routeFilter(transportList,mode){
+	routeMap[mode] ={}
+	var routeList = new Array()
+	for (var i = 0; i < transportList.length;i++ ){
+		var route = transportList[i].full[0].route
+		if(!(route in routeMap[mode])){
+			routeList[routeList.length]=route
+			routeMap[mode][route]=new Array()
+		} 
+		routeMap[mode][route][routeMap[mode][route].length] = transportList[i]
+	}
+	if(mode=='flight'){
+		flightRouteList=routeList
+	}
+	 
+}
