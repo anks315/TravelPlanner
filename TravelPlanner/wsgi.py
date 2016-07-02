7924ab09f214253d3a0a7b00114c1f448d0da8c3
anks315 <anks.315@gmail.com>
2016-07-02 16:21:26 +0530

    bug fix wsgi

--- TravelPlanner/wsgi.py
+++ TravelPlanner/wsgi.py
@@ -24,9 +24,12 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TravelPlanner.settings")
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
+
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
+import trainUtil as startup
+startup.loadtraindata()
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
 # application = HelloWorldApplication(application)
