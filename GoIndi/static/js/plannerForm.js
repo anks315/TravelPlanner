d6501b489d443975c5cdea2afe4279e6cab7fc57
anks315 <anks.315@gmail.com>
2016-07-02 22:51:53 +0530

    crossserver

--- GoIndi/static/js/plannerForm.js
+++ GoIndi/static/js/plannerForm.js
@@ -269,7 +269,7 @@ function getUrlVars() {
 					}
 				});
 
-	$.getJSON('https://q21sw0b7ai.execute-api.ap-southeast-1.amazonaws.com/dev/flight/bigtonear?sourcecity='+fromStation+'&sourcestate=&destinationcity='+toStation+'&destinationstate=&journeyDate='+depDate+"&flightClass="+flightClassSelected+"&trainClass="+trainClassSelected+"&adults="+persons, function(data, err) {
+	$.getJSON('https://tyk3pibkpd.execute-api.ap-southeast-1.amazonaws.com/dev/bigtonear?sourcecity='+fromStation+'&sourcestate=&destinationcity='+toStation+'&destinationstate=&journeyDate='+depDate+"&flightClass="+flightClassSelected+"&trainClass="+trainClassSelected+"&adults="+persons, function(data, err) {
 				  flightBigToNear =1
 				  if (err != "success") {
 					  
@@ -291,7 +291,7 @@ function getUrlVars() {
 				  }
 				});
 
-	$.getJSON('https://q21sw0b7ai.execute-api.ap-southeast-1.amazonaws.com/dev/flight/neartobig?sourcecity='+fromStation+'&sourcestate=&destinationcity='+toStation+'&destinationstate=&journeyDate='+depDate+"&flightClass="+flightClassSelected+"&trainClass="+trainClassSelected+"&adults="+persons, function(data, err) {
+	$.getJSON('https://tyk3pibkpd.execute-api.ap-southeast-1.amazonaws.com/dev/flight/neartobig?sourcecity='+fromStation+'&sourcestate=&destinationcity='+toStation+'&destinationstate=&journeyDate='+depDate+"&flightClass="+flightClassSelected+"&trainClass="+trainClassSelected+"&adults="+persons, function(data, err) {
 
 				  flightNearToBig=1
 				  if (err != "success") {
